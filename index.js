4affe02304601f5ded78193faa0050f99f90e7f9
Arham Raza <arhamraza399@gmail.com>
2023-06-30 09:05:01 +0500

    sidebar added

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-const sidebar2 = document.getElementById("sidebar")
-const openmenu = document.getElementById("openmenu")
-const closemenu = document.getElementById("closemenu")
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
 
-openmenu.addEventListener("click", () => {
-    sidebar.style.display = 'block'
-})
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
 
-closemenu.addEventListener("click", () => {
-    sidebar.style.display = "none"
-})
-
-
-const menuButton = document.querySelector(".menu");
-        const closeButton = document.querySelector(".close");
-        const sidebar = document.querySelector(".sidebar");
-
-        // Function to open the sidebar
-        function openSidebar() {
-            sidebar.classList.add("sidebar-open");
-        }
-
-        // Function to close the sidebar
-        function closeSidebar() {
-            sidebar.classList.remove("sidebar-open");
-        }
-
-        // Add event listeners to the menu and close buttons
-        menuButton.addEventListener("click", openSidebar);
-        closeButton.addEventListener("click", closeSidebar);
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
   
   
   
